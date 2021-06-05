9a5a892c9b2fc737bfed932750e1bdcb4483d184
Chony-Or <83386673+Chony-Or@users.noreply.github.com>
2021-06-06 03:34:01 +0800

    Add files via upload

---
+++ UIX/src/main/java/Passenger.java
@@ -0,0 +1,376 @@
+
+public class Passenger extends AIRLINE {
+    
+    //default icon, custom title
+    
+    //MANILA TO BATANES                         1
+    //regular
+    String[] regMBp = new String[47]; // Passenger Name
+    String[] regMBpclass = new String[47];  // Class
+    String[] regMBpAge = new String[47];  //Age
+    String[] regMBpfare = new String[47];  //Fare
+    String[] regMBpINS = new String[47]; //for insurance
+    //private
+    String[] priMBp = new String[13];
+    String[] priMBpclass = new String[13];
+    String[] priMBpAge = new String[13];
+    String[] priMBpfare = new String[13];
+    String[] priMBpINS = new String[47]; //for insurance
+    //business
+    String[] busMBp = new String[22];
+    String[] busMBpclass = new String[22];
+    String[] busMBpAge = new String[22];
+    String[] busMBpfare = new String[22];
+    String[] busMBpINS = new String[47]; //for insurance
+    
+//BATANES TO MANILA                         
+    //regular
+    String[] regBMp = new String[47];
+    String[] regBMpclass = new String[47];
+    String[] regBMpAge = new String[47];
+    String[] regBMpfare = new String[47];
+    String[] regBMpINS = new String[47]; //for insurance
+    //private
+    String[] priBMp = new String[13];
+    String[] priBMpclass = new String[13];
+    String[] priBMpAge = new String[13];
+    String[] priBMpfare = new String[13];
+    String[] priBMpINS = new String[47]; //for insurance
+    //business
+    String[] busBMp = new String[22];
+    String[] busBMpclass = new String[22];
+    String[] busBMpAge = new String[22];
+    String[] busBMpfare = new String[22];
+    String[] busBMpINS = new String[47]; //for insurance
+    
+    
+    //Manila to palawan                         2
+    //regular
+    String[] regMPp = new String[47];
+    String[] regMPpclass = new String[47];
+    String[] regMPpAge = new String[47];
+    String[] regMPpfare = new String[47];
+    String[] regMPpINS = new String[47]; //for insurance
+    //private
+    String[] priMPp = new String[13];
+    String[] priMPpclass = new String[13];
+    String[] priMPpAge = new String[13];
+    String[] priMPpfare = new String[13];
+    String[] priMPpINS = new String[47]; //for insurance
+    //business
+    String[] busMPp = new String[22];
+    String[] busMPpclass = new String[22];
+    String[] busMPpAge = new String[22];
+    String[] busMPpfare = new String[22];
+    String[] busMPpINS = new String[47]; //for insurance
+    //PALAWAN TO MANILA
+    //regular
+    String[] regPMp = new String[47];
+    String[] regPMpclass = new String[47];
+    String[] regPMpAge = new String[47];
+    String[] regPMpfare = new String[47];
+    String[] regPMpINS = new String[47]; //for insurance
+    //private
+    String[] priPMp = new String[13];
+    String[] priPMpclass = new String[13];
+    String[] priPMpAge = new String[13];
+    String[] priPMpfare = new String[13];
+    String[] priPMpINS = new String[47]; //for insurance
+    //business
+    String[] busPMp = new String[22];
+    String[] busPMpclass = new String[22];
+    String[] busPMpAge = new String[22];
+    String[] busPMpfare = new String[22];
+    String[] busPMpINS = new String[47]; //for insurance
+    
+    
+    //Manila to South Korea                     3
+    //regular
+    String[] regMSp = new String[47];
+    String[] regMSpclass = new String[47];
+    String[] regMSpAge = new String[47];
+    String[] regMSpfare = new String[47];
+    String[] regMSpINS = new String[47]; //for insurance
+    //private
+    String[] priMSp = new String[13];
+    String[] priMSpclass = new String[13];
+    String[] priMSpAge = new String[13];
+    String[] priMSpfare = new String[13];
+    String[] priMSpINS = new String[47]; //for insurance
+    //business
+    String[] busMSp = new String[22];
+    String[] busMSpclass = new String[22];
+    String[] busMSpAge = new String[22];
+    String[] busMSpfare = new String[22];
+    String[] busMSpINS = new String[47]; //for insurance
+    //South Korea TO MANILA
+    //regular
+    String[] regSMp = new String[47];
+    String[] regSMpclass = new String[47];
+    String[] regSMpAge = new String[47];
+    String[] regSMpfare = new String[47];
+    String[] regSMpINS = new String[47]; //for insurance
+    //private
+    String[] priSMp = new String[13];
+    String[] priSMpclass = new String[13];
+    String[] priSMpAge = new String[13];
+    String[] priSMpfare = new String[13];
+    String[] priSMpINS = new String[47]; //for insurance
+    //business
+    String[] busSMp = new String[22];
+    String[] busSMpclass = new String[22];
+    String[] busSMpAge = new String[22];
+    String[] busSMpfare = new String[22];
+    String[] busSMpINS = new String[47]; //for insurance
+    
+    
+    //Manila to Japan                           4
+    //regular
+    String[] regMJp = new String[47];
+    String[] regMJpclass = new String[47];
+    String[] regMJpAge = new String[47];
+    String[] regMJpfare = new String[47];
+    String[] regMJpINS = new String[47]; //for insurance
+    //private
+    String[] priMJp = new String[13];
+    String[] priMJpclass = new String[13];
+    String[] priMJpAge = new String[13];
+    String[] priMJpfare = new String[13];
+    String[] priMJpINS = new String[47]; //for insurance
+    //business
+    String[] busMJp = new String[22];
+    String[] busMJpclass = new String[22];
+    String[] busMJpAge = new String[22];
+    String[] busMJpfare = new String[22];
+    String[] busMJpINS = new String[47]; //for insurance
+    //JAPAN TO MANILA
+    //regular
+    String[] regJMp = new String[47];
+    String[] regJMpclass = new String[47];
+    String[] regJMpAge = new String[47];
+    String[] regJMpfare = new String[47];
+    String[] regJMpINS = new String[47]; //for insurance
+    //private
+    String[] priJMp = new String[13];
+    String[] priJMpclass = new String[13];
+    String[] priJMpAge = new String[13];
+    String[] priJMpfare = new String[13];
+    String[] priJMpINS = new String[47]; //for insurance
+    //business
+    String[] busJMp = new String[22];
+    String[] busJMpclass = new String[22];
+    String[] busJMpAge = new String[22];
+    String[] busJMpfare = new String[22];
+    String[] busJMpINS = new String[47]; //for insurance
+    
+    
+    //Manila to VIETNAM                         5
+    //regular
+    String[] regMVp = new String[47];
+    String[] regMVpclass = new String[47];
+    String[] regMVpAge = new String[47];
+    String[] regMVpfare = new String[47];
+    String[] regMVpINS = new String[47]; //for insurance
+    //private
+    String[] priMVp = new String[13];
+    String[] priMVpclass = new String[13];
+    String[] priMVpAge = new String[13];
+    String[] priMVpfare = new String[13];
+    String[] priMVpINS = new String[47]; //for insurance
+    //business
+    String[] busMVp = new String[22];
+    String[] busMVpclass = new String[22];
+    String[] busMVpAge = new String[22];
+    String[] busMVpfare = new String[22];
+    String[] busMVpINS = new String[47]; //for insurance
+    //VIETNAM TO MANILA
+    //regular
+    String[] regVMp = new String[47];
+    String[] regVMpclass = new String[47];
+    String[] regVMpAge = new String[47];
+    String[] regVMpfare = new String[47];
+    String[] regVMpINS = new String[47]; //for insurance
+    //private
+    String[] priVMp = new String[13];
+    String[] priVMpclass = new String[13];
+    String[] priVMpAge = new String[13];
+    String[] priVMpfare = new String[13];
+    String[] priVMpINS = new String[47]; //for insurance
+    //business
+    String[] busVMp = new String[22];
+    String[] busVMpclass = new String[22];
+    String[] busVMpAge = new String[22];
+    String[] busVMpfare = new String[22];
+    String[] busVMpINS = new String[47]; //for insurance
+    
+     
+}
+class lagaymo 
+{
+    Passenger p = new Passenger();
+    Destination INS = new Destination();
+//     if (d==0)//MANILA TO BATANES
+//        {
+//          if (classbox.getText()=="REGULAR CLASS")
+//          {
+//          p.regVMpINS[regMBcount] = INS.priInsurance;
+//          }
+//          
+//          if (classbox.getText()=="PRIVATE CLASS")
+//          {
+//     
+//          }
+//          if (classbox.getText()=="BUSINESS CLASS")
+//          {
+//        
+//          }
+//        }
+//        if (d==1)//BATANES TO MANILA
+//        {
+//          if (classbox.getText()=="REGULAR CLASS")
+//          {
+//         
+//          }
+//          
+//          if (classbox.getText()=="PRIVATE CLASS")
+//          {
+//        
+//          }
+//          if (classbox.getText()=="BUSINESS CLASS")
+//          {
+//     
+//          }
+//        }
+//        
+//        
+//        if (d==2) //MANILA TO PALAWAN
+//        {
+//          if (classbox.getText()=="REGULAR CLASS")
+//          {
+//       
+//          }
+//          
+//          if (classbox.getText()=="PRIVATE CLASS")
+//          {
+//       
+//          }
+//          if (classbox.getText()=="BUSINESS CLASS")
+//          {
+//    
+//          }
+//        }
+//        if (d==3) //PALAWAN TO MANILA
+//        {
+//          if (classbox.getText()=="REGULAR CLASS")
+//          {
+//        
+//          }
+//          
+//          if (classbox.getText()=="PRIVATE CLASS")
+//          {
+//         
+//          }
+//          if (classbox.getText()=="BUSINESS CLASS")
+//          {
+//        
+//          }
+//        }
+//        if (d==4) //MANILA TO SOUTH KOREA
+//        {
+//          if (classbox.getText()=="REGULAR CLASS")
+//          {
+//      
+//          }
+//          
+//          if (classbox.getText()=="PRIVATE CLASS")
+//          {
+//      
+//          }
+//          if (classbox.getText()=="BUSINESS CLASS")
+//          {
+//      
+//          }
+//        }
+//        if (d==5) //SOUTH KOREA TO MANILA
+//        {
+//          if (classbox.getText()=="REGULAR CLASS")
+//          {
+//     
+//          }
+//          
+//          if (classbox.getText()=="PRIVATE CLASS")
+//          {
+//    
+//          }
+//        }
+//        if (d==6) //MANILA TO JAPAN
+//        {
+//          if (classbox.getText()=="REGULAR CLASS")
+//          {
+//        
+//          }
+//          
+//          if (classbox.getText()=="PRIVATE CLASS")
+//          {
+//
+//          }
+//          if (classbox.getText()=="BUSINESS CLASS")
+//          {
+//     
+//          }
+//        }
+//        if (d==7) //JAPAN TO MANILA
+//        {
+//          if (classbox.getText()=="REGULAR CLASS")
+//          {
+//       
+//          }
+//          
+//          if (classbox.getText()=="PRIVATE CLASS")
+//          {
+//
+//          }
+//          if (classbox.getText()=="BUSINESS CLASS")
+//          {
+//  
+//          }
+//        }
+//        if (d==8) //MANILA TO VIETNAM
+//        {
+//          if (classbox.getText()=="REGULAR CLASS")
+//          {
+//    
+//          }
+//          
+//          if (classbox.getText()=="PRIVATE CLASS")
+//          {
+//   
+//          }
+//          if (classbox.getText()=="BUSINESS CLASS")
+//          {
+//              
+//          }
+//        }
+//        if (d==9) //VIETNAM TO MANILA
+//        {
+//          if (classbox.getText()=="REGULAR CLASS")
+//          {
+//      
+//          }
+//          
+//          if (classbox.getText()=="PRIVATE CLASS")
+//          {
+//
+//          }
+//          if (classbox.getText()=="BUSINESS CLASS")
+//          {
+// 
+//          }
+//        }
+//        
+//     
+//        MAINPanel.setVisible(false);
+//        PassengerPanel1.setVisible(false);
+//        DestinationPanel.setVisible(false); 
+//        TransactionPanel.setVisible(true);
+}
---
+++ UIX/src/main/java/Transaction.java
@@ -0,0 +1,71 @@
+
+public class Transaction extends AIRLINE{
+    float priBagfee = 1250;
+    float busBagfee = 2850;
+    float regBagfee = 950;
+    
+    
+    float[] bagFee  = {950,1250,2850};
+    float[][] bagFeeList = {
+                           bagFee,
+                           bagFee,
+                           bagFee,
+                           bagFee,
+                           bagFee,
+                           bagFee,
+                           bagFee,
+                           bagFee,
+                           bagFee,
+                           bagFee,       
+                        };
+    
+//    float BUSTRAVTCHARGE = 5700;
+//    float PRTRAVTCHARGE = 4260;
+//    float REGTRAVTCHARGE = 2500;
+
+    float[] travCharge  = {2500,4260,5700};
+    float[][] travChargeList = {
+                            travCharge,
+                            travCharge,
+                            travCharge,
+                            travCharge,
+                            travCharge,
+                            travCharge,
+                            travCharge,
+                            travCharge,
+                            travCharge,
+                            travCharge,       
+                        };
+    
+//    float regInsurance = 950;
+//    float priInsurance = 4500;
+//    float busInsurance = 6500;
+    
+    float[] Insurance  = {950,4500,6500};
+    float[][] InsuranceList = {
+                            Insurance,
+                            Insurance,
+                            Insurance,
+                            Insurance,
+                            Insurance,
+                            Insurance,
+                            Insurance,
+                            Insurance,
+                            Insurance,
+                            Insurance,        
+                        };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
